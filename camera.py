db3a20619acb1f191e7a2c959b18232c7290a696
Onur Öğüt <52832895+onurogut@users.noreply.github.com>
2022-01-17 04:00:42 +0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -54,7 +54,7 @@ def markAttendance(name):
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 class Video(object):
     def __init__(self):
-        self.video=cv2.VideoCapture(1)
+        self.video=cv2.VideoCapture(-1)
     def __del__(self):
         self.video.release()
     def get_frame(self):
